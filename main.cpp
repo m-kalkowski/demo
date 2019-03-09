4afced7539dc5f352bd635935e01e82dc91fd886
Mateusz Kalkowski <kalkowski_m@wp.pl>
2019-03-09 15:10:25 +0100

    add images to buttons - opening ui

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setOverrideCursor(QCursor(Qt::BlankCursor));
     MainWindow w;
     w.showMaximized();
 
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,33 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QPixmap pixmap;
+    QIcon ButtonIcon;
+
+    pixmap.load(":/btn1.jpg");
+    ButtonIcon.addPixmap(pixmap);
+    ui->pushButton->setIcon(ButtonIcon);
+    ui->pushButton->setIconSize(pixmap.rect().size());
+    ui->pushButton->setFixedSize(pixmap.rect().size());
+
+    pixmap.load(":/btn2.jpg");
+    ButtonIcon.addPixmap(pixmap);
+    ui->pushButton_2->setIcon(ButtonIcon);
+    ui->pushButton_2->setIconSize(pixmap.rect().size());
+    ui->pushButton_2->setFixedSize(pixmap.rect().size());
+
+    pixmap.load(":/btn3.jpg");
+    ButtonIcon.addPixmap(pixmap);
+    ui->pushButton_3->setIcon(ButtonIcon);
+    ui->pushButton_3->setIconSize(pixmap.rect().size());
+    ui->pushButton_3->setFixedSize(pixmap.rect().size());
+
+    pixmap.load(":/btn4.jpg");
+    ButtonIcon.addPixmap(pixmap);
+    ui->pushButton_4->setIcon(ButtonIcon);
+    ui->pushButton_4->setIconSize(pixmap.rect().size());
+    ui->pushButton_4->setFixedSize(pixmap.rect().size());
 }
 
 MainWindow::~MainWindow()
@@ -15,5 +42,5 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    ui->label->setText("Hello Inwebit");
+
 }
