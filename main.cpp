bdc1a502d1fac9a8a7ff1878c8092daa25427e79
Mateusz Kalkowski <kalkowski_m@wp.pl>
2019-03-09 15:36:13 +0100

    add background color

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setOverrideCursor(QCursor(Qt::BlankCursor));
     MainWindow w;
+    w.setStyleSheet("background-color:#343434;");
     w.showMaximized();
 
     return a.exec();
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,25 +9,6 @@ MainWindow::MainWindow(QWidget *parent) :
 
     QPixmap pixmap;
     QIcon ButtonIcon;
-
-    pixmap.load(":/btn1.jpg");
-    ButtonIcon.addPixmap(pixmap);
-    ui->pushButton->setIcon(ButtonIcon);
-    ui->pushButton->setIconSize(pixmap.rect().size());
-    ui->pushButton->setFixedSize(pixmap.rect().size());
-
-    pixmap.load(":/btn2.jpg");
-    ButtonIcon.addPixmap(pixmap);
-    ui->pushButton_2->setIcon(ButtonIcon);
-    ui->pushButton_2->setIconSize(pixmap.rect().size());
-    ui->pushButton_2->setFixedSize(pixmap.rect().size());
-
-    pixmap.load(":/btn3.jpg");
-    ButtonIcon.addPixmap(pixmap);
-    ui->pushButton_3->setIcon(ButtonIcon);
-    ui->pushButton_3->setIconSize(pixmap.rect().size());
-    ui->pushButton_3->setFixedSize(pixmap.rect().size());
-
     pixmap.load(":/btn4.jpg");
     ButtonIcon.addPixmap(pixmap);
     ui->pushButton_4->setIcon(ButtonIcon);
